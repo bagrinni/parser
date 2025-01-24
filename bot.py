abaa3e5be36c0795b31befb46818549b300e0d3d
bagrinni <simka.ws@mail.ru>
2025-01-25 01:39:48 +0600

    Добавлен порт2

--- bot.py
+++ bot.py
@@ -137,6 +137,4 @@ async def main():
     await dispatcher.start_polling(bot)
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-
+    asyncio.run(main())
